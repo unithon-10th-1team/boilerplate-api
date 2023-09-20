9c76172616678923513c5e2a533de0a85f9471a5
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-09-21 02:57:18 +0900

    init

---
+++ src/main/java/com/goofy/boilerplate/SpringMvcBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.goofy.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBoilerplateApplication.class, args);
+	}
+
+}
