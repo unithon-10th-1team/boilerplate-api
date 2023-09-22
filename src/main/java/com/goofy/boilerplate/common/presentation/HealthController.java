4b5ab59be64ba2904ca02f23c489b974632f7eb1
kdomo <dongho5088@naver.com>
2023-09-23 00:27:46 +0900

    style: spotless Apply

--- src/main/java/com/goofy/boilerplate/common/presentation/HealthController.java
+++ src/main/java/com/goofy/boilerplate/common/presentation/HealthController.java
@@ -1,12 +1,11 @@
 package com.goofy.boilerplate.common.presentation;
 
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 @RestController
 @RequestMapping("/health-check")
