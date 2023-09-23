cc8aecebafc632228d0bd42edbbda4eb80b4b262
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-09-24 03:24:17 +0900

    feat: ott user select

--- src/main/java/com/flickspick/ott/service/OttService.java
+++ src/main/java/com/flickspick/ott/service/OttService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,5 +44,9 @@ public class OttService {
                         .ottId(ott.getId())
                         .build()
         );
+    }
+
+    public List<OttUser> findAll(List<Long> ids) {
+        return ottUserRepository.findAllById(ids);
     }
 }
