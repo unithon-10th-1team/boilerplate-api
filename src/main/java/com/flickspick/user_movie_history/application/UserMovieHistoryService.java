dbb8dbde4d26b7164efa55ee410a574cc3a3f459
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-09-24 04:29:05 +0900

    fix: me api는 언제든 사용 가능~

--- src/main/java/com/flickspick/user_movie_history/application/UserMovieHistoryService.java
+++ src/main/java/com/flickspick/user_movie_history/application/UserMovieHistoryService.java
@@ -1,26 +1,31 @@
 package com.flickspick.user_movie_history.application;
 
-import org.springframework.stereotype.Service;
-
 import com.flickspick.exception.dto.ErrorType;
 import com.flickspick.exception.user_movie_history.UserMovieHistoryNotFoundException;
-import com.flickspick.rec.dto.RecResponse;
 import com.flickspick.user_movie_history.domain.UserMovieHistory;
 import com.flickspick.user_movie_history.infrastructure.UserMovieHistoryRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserMovieHistoryService {
-	private final UserMovieHistoryRepository userMovieHistoryRepository;
+    private final UserMovieHistoryRepository userMovieHistoryRepository;
 
-	public UserMovieHistory saveUserMovieHistory(UserMovieHistory userMovieHistory) {
-		return userMovieHistoryRepository.save(userMovieHistory);
-	}
+    @Transactional
+    public UserMovieHistory saveUserMovieHistory(UserMovieHistory userMovieHistory) {
+        return userMovieHistoryRepository.save(userMovieHistory);
+    }
 
-	public UserMovieHistory getRecentHistory(Long userId) {
-		return userMovieHistoryRepository.findTopByUserIdOrderByCreatedAtDesc(userId)
-				.orElseThrow(() -> new UserMovieHistoryNotFoundException(ErrorType.USER_MOVIE_HISTORY_NOT_FOUND));
-	}
+    public UserMovieHistory getRecentHistory(Long userId) {
+        var userMovieHistory = userMovieHistoryRepository.findTopByUserIdOrderByCreatedAtDesc(userId);
+
+        if (userMovieHistory.isEmpty()) {
+            return userMovieHistoryRepository.findTop1ByOrderByCreatedAtDesc()
+                    .orElseThrow(() -> new UserMovieHistoryNotFoundException(ErrorType.USER_MOVIE_HISTORY_NOT_FOUND));
+        }
+
+        return userMovieHistory.get();
+    }
 }
--- src/main/java/com/flickspick/user_movie_history/infrastructure/UserMovieHistoryRepository.java
+++ src/main/java/com/flickspick/user_movie_history/infrastructure/UserMovieHistoryRepository.java
@@ -1,13 +1,14 @@
 package com.flickspick.user_movie_history.infrastructure;
 
-import java.util.Optional;
-
+import com.flickspick.user_movie_history.domain.UserMovieHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.flickspick.user_movie_history.domain.UserMovieHistory;
+import java.util.Optional;
 
 public interface UserMovieHistoryRepository extends JpaRepository<UserMovieHistory, Long> {
-	Optional<UserMovieHistory> findByRecommendTypeIdAndMovieId(Long recommendTypeId, Long movieId);
-	Optional<UserMovieHistory> findTopByUserIdOrderByCreatedAtDesc(Long userId);
+    Optional<UserMovieHistory> findByRecommendTypeIdAndMovieId(Long recommendTypeId, Long movieId);
 
+    Optional<UserMovieHistory> findTopByUserIdOrderByCreatedAtDesc(Long userId);
+
+    Optional<UserMovieHistory> findTop1ByOrderByCreatedAtDesc();
 }
